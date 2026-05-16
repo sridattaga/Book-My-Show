c150a20a8313642ecc9d58cd5045ab2617e26d27
sridattaga <sivag.soc95@gmail.com>
2026-05-16 13:12:36 +0530

    Create subnets.tf

---
+++ terraform/subnets.tf
@@ -0,0 +1,7 @@
+resource "aws_subnet" "public" {
+  count = 2
+  vpc_id = aws_vpc.bms_vpc.id
+  cidr_block = cidrsubnet("10.0.0.0/16", 8, count.index)
+  availability_zone = element(["ap-southeast-1a","ap-southeast-1b"], count.index)
+  map_public_ip_on_launch = true
+}
