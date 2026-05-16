4b3010e62d76b949888981377d0edee427045d84
sridattaga <sivag.soc95@gmail.com>
2026-05-16 15:54:30 +0530

    Add AWS LB target group resource configuration

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -4,3 +4,12 @@ resource "aws_lb" "app_alb" {
   subnets            = var.public_subnets
   security_groups    = [var.alb_sg]
 }
+
+resource "aws_lb_target_group" "tg" {
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  health_check {
+    path = "/"
+  }
+}
